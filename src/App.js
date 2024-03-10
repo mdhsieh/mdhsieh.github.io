8af621a9ad95851fb578493eaa321a2a6df01c34
Michael Hsieh <mdhsieh8@gmail.com>
2024-03-09 16:48:28 -0800

    refactor: Remove unused resume data and fetching.

--- src/App.js
+++ src/App.js
@@ -8,32 +8,11 @@ class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      websiteData: {}
-    };
-
     ReactGA.initialize('UA-110570651-1');
     ReactGA.pageview(window.location.pathname);
-
-  }
-
-  getwebsiteData(){
-    $.ajax({
-      url:'/websiteData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({websiteData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
   }
 
   componentDidMount(){
-    this.getwebsiteData();
   }
 
   render() {
